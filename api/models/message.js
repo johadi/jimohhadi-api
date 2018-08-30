decd78cf9404fd4623ae2fb29f9f896734520ad1
johadi <jimoh.hadi@gmail.com>
2018-08-30 12:33:26 +0100

    modify message schema

--- api/models/message.js
+++ api/models/message.js
@@ -5,6 +5,9 @@ const messageSchema = new mongoose.Schema({
   email: {type: String, required: true, lowercase: true},
   subject: {type: String, required: true, lowercase: true},
   message: {type: String, required: true, lowercase: true},
+  read: {type: Number, default: 0}
+}, {
+  timestamps: true
 });
 
 export const Message = mongoose.model('Message', messageSchema);
